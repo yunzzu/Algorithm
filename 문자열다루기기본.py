b41ce940b9766effcd84a5567995ce5f54600dc4
yunzzu <81916114+yunzzu@users.noreply.github.com>
2023-08-11 02:17:16 +0900

    230810 prac

--- 문자열다루기기본.py
+++ 문자열다루기기본.py
@@ -1,8 +1,11 @@
 def solution(s):
-    res = True
+    res = False
     a = [chr(97+i) for i in range(26)] + [chr(65+i) for i in range(26)]
-    for i in s:
-        if i in a:
-            res = False 
-            break 
+    if len(s)==4 or len(s)==6: #조건 추가
+        for i in s:
+            if i not in a:
+                res = True
+            else:
+                res = False
+                break
     return res 
